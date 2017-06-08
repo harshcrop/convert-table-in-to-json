0289c7a3b03176c0f45564ca76969b96502568e0
harshcrop <harshcrop@gmail.com>
2017-06-08 16:27:20 +0530

    second commit adding files

---
+++ tabletojson.js
@@ -0,0 +1,12 @@
+var myRows = [];
+var $headers = $("th");
+var $rows = $("tbody tr").each(function(index) {
+  $cells = $(this).find("td");
+  myRows[index] = {};
+  $cells.each(function(cellIndex) {
+    myRows[index][$($headers[cellIndex]).html()] = $(this).html();
+  });    
+});
+var myObj = {};
+myObj.myrows = myRows;
+alert(JSON.stringify(myObj));
